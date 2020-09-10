32e27c4703e9916ce5582fad3c32df5812ec6331
Rishavrishu-creator <66913014+Rishavrishu-creator@users.noreply.github.com>
2020-09-11 05:11:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -63,14 +63,18 @@ app.get('/contact',function(req,res){
 })
 
 app.post('/email',function(req,res){
+    var auth = {
+        type: 'oauth2',
+        user: 'rishavrishu2001.ra@gmail.com',
+        clientId: '739935140116-3frgnjh2puimklj54fkb9rir1puti6pa.apps.googleusercontent.com',
+        clientSecret: 'J9JY-1zOv7npuhqZGcKTfsYE',
+        refreshToken: '1//04nz80eCyIN3bCgYIARAAGAQSNwF-L9Irazy1flm0tspU3-EFoJLMDBxtLe_0v2R3ikogn8FsS2ZNdjivgIYn-xVBBTL8DhQtdlw',
+    };
 
     var transport = nodeMailer.createTransport({
         service:'gmail',
-        host: 'smtp.gmail.com',
-        auth:{
-            user:'rishavrishu2001.ra',
-            pass :process.env.Password  
-        }
+        auth:auth
+       
 })
 
    
