fb66acdd9d95555f598b8da0af082f032c796f24
Rishavrishu-creator <66913014+Rishavrishu-creator@users.noreply.github.com>
2020-09-11 04:44:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ app.post('/email',function(req,res){
     var transport = nodeMailer.createTransport({
         service:'gmail',
         auth:{
-            user:'rishavrishu2001.ra@gmail.com',
+            user:'rishavrishu2001.ra',
             pass :process.env.Password  
         }
 })
